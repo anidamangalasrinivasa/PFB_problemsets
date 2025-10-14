d1e4e569fdbc6495749d56dc64924c5041023465
Ananya Nidamangala Srinivasa <ananyans197@gmail.com>
2025-10-14 11:03:45 -0400

    Adding python problemset1

---
+++ about_me.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+
+name='Ananya'
+print(f"My name: {name}")
+
+fave_color=sys.argv[1]
+print('My favorite color:' + fave_color)
+
+#fave_act='dancing'
+fave_act=sys.argv[2]
+print('My favorite activity:'+ fave_act)
+
+#fave_animal='Cat'
+fave_animal=sys.argv[3]
+print('My favorite animal:' + fave_animal)
